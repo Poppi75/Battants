c7232ec049e534891115fa1edb709fc7a99229ae
Poppi75 <kananenjohannes@gmail.com>
2026-01-12 22:21:06 +0200

    cant remember what i did

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var play_button: BaseButton = $VBoxContainer/PlayButton
+@onready var settings_button: BaseButton = $VBoxContainer/SettingsButton
+@onready var quit_button: BaseButton = $VBoxContainer/QuitButton
+
+func _ready():
+	play_button.pressed.connect(_on_play_pressed)
+	settings_button.pressed.connect(_on_settings_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/maps/dev_test_map.tscn")
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("")
+
+func _on_quit_pressed():
+	get_tree().quit()
