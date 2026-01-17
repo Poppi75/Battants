cd2e095f9d76fc30c09d338963814cdb1fd6aa04
bazzdragon <ossi.heikkinen5000@gmail.com>
2026-01-18 00:30:46 +0200

    tää ei tullu

--- global.gd
+++ global.gd
@@ -1,3 +1,9 @@
 extends Node
 
 var player_bindings: Array = []
+
+var maps: Array[String] = [
+	"res://scenes/maps/dev_test_map_test.tscn",
+	"res://scenes/maps/map1.tscn",
+	"res://scenes/maps/map2.tscn",
+]
