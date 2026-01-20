da3f82011eb90eb266e81edc1e105f7c75237c73
Ossi Eemeli Heikkinen <s4heos00@students.osao.fi>
2026-01-20 09:39:24 +0200

    coinspawner ja muihinki mappeihin winner teksti

---
+++ scripts/items/coinspawner.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+@export var coin_scene: PackedScene = preload("res://scenes/items/random_pickup_coin.tscn")
+@export var max_attempts := 20
+
+func _ready():
+	await get_tree().process_frame
+	randomize()
+	$Timer.start()
+	
+func spawn_coin():
+	for i in max_attempts:
+		var pos = get_random_point_in_area()
+		if is_position_free(pos):
+			var coin = coin_scene.instantiate()
+			coin.global_position = pos
+			get_tree().current_scene.add_child(coin)
+			return
+
+func get_random_point_in_area() -> Vector2:
+	var shape := $CollisionShape2D.shape as RectangleShape2D
+	var extents = shape.extents
+	return global_position + Vector2(
+		randf_range(-extents.x, extents.x),
+		randf_range(-extents.y, extents.y)
+	)
+
+func is_position_free(pos: Vector2) -> bool:
+	var space_state = get_world_2d().direct_space_state
+	var query := PhysicsPointQueryParameters2D.new()
+	query.position = pos
+	query.exclude = [self]
+	query.collide_with_bodies = true
+	query.collide_with_areas = true
+	return space_state.intersect_point(query).is_empty()
+
+
+func _on_timer_timeout() -> void:
+	spawn_coin()
