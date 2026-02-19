14ba85415a6c7a39b8ca915e966e5fd1194e843b
Poppi75 <kananenjohannes@gmail.com>
2026-02-19 14:03:36 +0200

    fixed homing rocket not exploding on hit with shield

--- scripts/weapons/bullet_hominglauncher.gd
+++ scripts/weapons/bullet_hominglauncher.gd
@@ -67,6 +67,17 @@ func _explode_now() -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
+	if explosion_scene == null:
+		print("no explosion scene :((")
+		queue_free()
+		return
+
+	var explosion = explosion_scene.instantiate()
+	get_tree().current_scene.add_child(explosion)
+
+	explosion.global_position = global_position
+	explosion.damage_amount = damage_amount
+	
 	if area.is_in_group("shield"):
 		queue_free()
 	
