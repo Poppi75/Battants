39cbc9ca18cb2602b2cb96ce9d129c35c17491e8
Ossi Eemeli Heikkinen <s4heos00@students.osao.fi>
2026-02-24 09:39:16 +0200

    dink to everything

--- scripts/weapons/sawedoff_bullet.gd
+++ scripts/weapons/sawedoff_bullet.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var auto_free_time: float = 0.42 # pellets despawn fast
 
 @export var damage_amount: int = 6
+@export var headshot_damage: int = 7
+@onready var dink_sound: AudioStreamPlayer2D = $dink_sound
 
 # Optional: extra variation so pellets don't form a perfect arc
 @export var speed_variance: float = 120.0 # 0 = disabled
@@ -31,7 +33,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("players"):
-		body.take_damage(damage_amount)
+		body.take_damage(damage_amount, false)
 
 	queue_free()
 
@@ -40,5 +42,29 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shield"):
 		queue_free()
 	
-	else:
-		return
+	if area.name == "HeadshotArea":
+		var player := area.get_parent()
+
+		if player.is_in_group("players"):
+			var damage = headshot_damage
+			play_dink_sound()
+
+			print("[Bullet] HEADSHOT for %d" % damage_amount)
+			player.take_damage(damage, true)
+
+			queue_free()  # Bullet can die immediately
+
+
+func play_dink_sound() -> void:
+	var sound_player := AudioStreamPlayer2D.new()
+	sound_player.stream = dink_sound.stream
+	sound_player.position = global_position
+	sound_player.pitch_scale = randf_range(0.95, 1.05)
+	sound_player.autoplay = false
+
+	# Add to scene root so it's independent of the bullet
+	get_tree().current_scene.add_child(sound_player)
+
+	# Play and free when done
+	sound_player.play()
+	sound_player.finished.connect(Callable(sound_player, "queue_free"))
