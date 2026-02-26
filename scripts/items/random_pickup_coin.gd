450372b086cbde496c1928e6ce548670d24d297c
bazzdragon <ossi.heikkinen5000@gmail.com>
2026-02-26 11:52:00 +0200

    pickup input and placeholder pickup chance indicator

--- scripts/items/random_pickup_coin.gd
+++ scripts/items/random_pickup_coin.gd
@@ -4,19 +4,49 @@ extends Area2D
 @onready var sprite_2d_2: Sprite2D = $Sprite2D2
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-var cant_enter = true
+var claimed: bool = false
+var players_in_range: Array[Player] = []
+
+func _process(_delta: float) -> void:
+	if claimed:
+		return
+
+	for p in players_in_range:
+		if not is_instance_valid(p):
+			continue
+		if p.is_pickup_pressed():
+			_claim(p)
+			return
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("players") and cant_enter == true:
-		print("[Pickup Coin] Player body entered")
-		cant_enter = false
-		
-		body.pickup()
-		
-		sprite_2d_2.visible = false
-		collision_shape_2d.set_deferred("disable_mode", true)
-		
-		pickup_sound.play()
-		await pickup_sound.finished
-		
-		queue_free()
+	if claimed:
+		return
+	if body is Player and body.is_in_group("players"):
+		if not players_in_range.has(body):
+			players_in_range.append(body)
+			body._pickup_area_entered()
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		players_in_range.erase(body)
+		body._pickup_area_exited()
+
+func _claim(p: Player) -> void:
+	if claimed:
+		return
+	claimed = true
+
+	# Hide hint for everyone still in range (since the coin is gone)
+	for pl in players_in_range:
+		if is_instance_valid(pl):
+			pl._pickup_area_exited()
+	players_in_range.clear()
+
+	p.pickup()
+
+	sprite_2d_2.visible = false
+	collision_shape_2d.set_deferred("disabled", true)
+
+	pickup_sound.play()
+	await pickup_sound.finished
+	queue_free()
